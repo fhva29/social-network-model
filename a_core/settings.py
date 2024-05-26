3572e2fa459df965064ca66646249917f7076200
fhva29 <fhv.araujo@gmail.com>
2024-05-26 13:09:53 -0300

    feat: update on databases

--- a_core/settings.py
+++ a_core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from environ import Env
+import dj_database_url
 
 env = Env()
 Env.read_env()
@@ -108,6 +109,10 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+POSTGRES_LOCALLY = False
+if ENVIRONMENT == "production" or POSTGRES_LOCALLY == True:
+    DATABASES["default"] = dj_database_url.parse(env("DATABASE_URL"))
 
 
 # Password validation
